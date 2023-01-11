649203b14a389fa7b41e12c4c0c2ae76184b5a4f
801txchii <sdgaletech@gmail.com>
2023-01-11 13:51:04 -0800

    p

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { page } from '$app/stores';
 </script>
 
 <div class="app">
@@ -13,6 +14,25 @@
 	<footer>
 		<h3>&copy 2023 SJRT</h3>
 		
+		<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/sverdle' ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
 	</footer>
 </div>
 
