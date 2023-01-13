804d7472ca4ff22ed90b6643aa6b633ec287f8e5
801 Txchii <sdgaletech@gmail.com>
2023-01-13 16:00:26 -0700

    ok

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,11 +1,28 @@
 <script>
-    const correct = () => {
-        
-    }
+    import {sentences} from './sentences.js'
+
+    let numberOfGuesses = 1;
+    const maxGuesses = 5;
+    let guessesRemaining = numberOfGuesses;
+    let currentGuess = [];
+    let rightGuessString = sentences[Math.floor(Math.random * sentences.length)];
+
+    let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
 </script>
 
 <main>
 
+<ul>
+	{#each cats as { id, name }, i}
+		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}" rel="noreferrer">
+			{i + 1}: {name}
+		</a></li>
+	{/each}
+</ul>
 </main>
 
 <style>
---
+++ src/routes/sentences.js
@@ -0,0 +1,11 @@
+export const sentences = [
+    'hello world',
+    'welcome to the game',
+    'this is fun',
+    'the quick brown fox jumps over the lazy dog',
+    'start your engines',
+    'nasa, we have a problem',
+    'hello from outer space',
+    'thank you for playing this game',
+    'i wonder what this year will be like'
+]
