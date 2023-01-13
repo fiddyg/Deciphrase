d3b3b64e4a3bc1aa7f733f9b1cd5b1fd17c7a0f7
801 Txchii <sdgaletech@gmail.com>
2023-01-13 16:36:27 -0700

    yaa

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,18 +1,25 @@
-<script>
-	let cats = [
-		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
-		{ id: 'z_AbfPXTKms', name: 'Maru' },
-		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+<script>	
+	let players = [
+		{ img: 'profileImg/me.png', name: 'Spencer Gale', points: 25 },
+		{ name: 'Maru' },
+		{ name: 'Henri The Existential Cat' }
 	];
 </script>
 
 <main>
 
 <ul>
-	{#each cats as { id, name }, i}
-		<li><a target="_blank" href="https://www.youtube.com/watch?v={id}" rel="noreferrer">
-			{i + 1}: {name}
-		</a></li>
+	{#each players as { img, name, points }, i}
+		<div class="person">
+			<span>
+				<h4>{ i + 1 }: </h4>
+				{img} 
+				<h4>{name}</h4> 
+				<p>{points}</p>
+			</span>
+		</div>
+
+
 	{/each}
 </ul>
 </main>
@@ -23,4 +30,22 @@
 		align-items: center;
 		justify-content: center;
 	}
+
+	.person {
+		border: 2px solid white;
+		width: 100%;
+	}
+
+	span {
+		margin: 0 1rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h4, p {
+		display: inline;
+	}
+
+
 </style>
