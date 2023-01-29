b4fe918078cf7458445a0c7d479965da7bdb2151
801 Txchii <sdgaletech@gmail.com>
2023-01-29 10:17:52 -0700

    l

--- src/routes/HowToPlay.svelte
+++ src/routes/HowToPlay.svelte
@@ -10,23 +10,7 @@
     <h2 slot="header">How To Play</h2>
 
     <p>
-      This game... Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Fuga, molestiae, dolore eveniet aut debitis facere voluptatem, ad
-      perferendis provident suscipit quae deserunt omnis delectus amet. Quam
-      sunt perspiciatis debitis reprehenderit. Nam vero repudiandae, aut
-      nesciunt itaque sed! Atque dignissimos illum itaque esse adipisci vitae
-      pariatur harum cupiditate ratione nihil tempora sit omnis, velit unde
-      corrupti, ex qui explicabo, illo aspernatur? Blanditiis numquam deleniti
-      quidem vitae quas atque voluptas nam a amet cum iure voluptatem eligendi
-      ullam perspiciatis corporis assumenda maiores, ab illum nisi laborum
-      similique delectus, nihil, veritatis vel! Harum. Et, soluta, saepe quam
-      pariatur harum quae tempora labore a dolore fugit repellat perspiciatis
-      porro voluptatum quos repellendus omnis earum aliquam assumenda,
-      repudiandae beatae debitis. Consequatur labore laboriosam quod nisi?
-      Totam, expedita. Odio, facere cupiditate totam necessitatibus ducimus,
-      obcaecati debitis iste modi incidunt, quisquam sint. Odit iusto placeat
-      aspernatur tempora, eum fugit pariatur excepturi aperiam iste nisi neque
-      in. Repudiandae.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit quas asperiores necessitatibus, similique velit ea, facere possimus perferendis a aut voluptatem dolorem quod dicta harum corrupti pariatur rerum, maxime cupiditate.
     </p>
   </Modal>
 {/if}
