1b75adfb1319edf205c30cbbf2de0c713d4f06ad
801txchii <sdgaletech@gmail.com>
2023-02-01 14:12:06 -0800

    yayo

--- src/routes/sentences.js
+++ src/routes/sentences.js
@@ -4,7 +4,7 @@ export const sentences = [
     'this is fun',
     'the quick brown fox jumps over the lazy dog',
     'start your engines',
-    'nasa, we have a problem',
+    'houston, we have a problem',
     'hello from outer space',
     'thank you for playing this game',
     'i wonder what this year will be like',
