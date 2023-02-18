bc828687358400b455dbb49904fdae0b53062fad
801 Txchii <sdgaletech@gmail.com>
2023-02-17 19:03:56 -0700

    ?

---
+++ src/routes/leaderboard/auth/Login.svelte
@@ -0,0 +1,14 @@
+<script>
+    import SignUp from "src/routes/SignUp.svelte";
+    import LogIn from "./Login.svelte";
+</script>
+
+<main>
+    <h1>Login</h1>
+
+    <LogIn />
+
+    <div class="signin">
+        <h3>Already have an account? <a href="./SignUp.svelte">Sign Up</a></h3>
+    </div>
+</main>
