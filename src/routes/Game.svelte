8e0fccff4977b70cee080f1a447675cacaae42b4
801txchii <sdgaletech@gmail.com>
2023-03-13 15:37:45 -0700

    hmm

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -2,20 +2,7 @@
 	import HowToPlay from './HowToPlay.svelte'
 	import {phrases} from './phrases.js'
  
-	function shuffle(array) {
-		let currentIndex = array.length, randomIndex;
-
-		while (currentIndex != 0) {
-			randomIndex = Math.floor(Math.random() * currentIndex)
-			currentIndex--
-
-			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
-		}
-
-		return array
-	}
-
-	shuffle(phrases)
+	
 
 	let length = phrases.length;
 
--- src/routes/phrases.js
+++ src/routes/phrases.js
@@ -1,3 +1,17 @@
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+    }
+
+    return array
+}
+
+
 export const phrases = [
     'h',
     'e',
@@ -5,3 +19,26 @@ export const phrases = [
     'l',
     'o'
 ]
+
+function phrase1() {
+    const word1letters = [
+        'h',
+        'e',
+        'l',
+        'l',
+        'o'
+    ]
+
+    const word1 = shuffle(word1letters)
+
+    const word2letters = [
+        'w',
+        'o',
+        'r',
+        'l',
+        'd'
+    ]
+
+    const word2 = shuffle(word2letters)
+    
+}
