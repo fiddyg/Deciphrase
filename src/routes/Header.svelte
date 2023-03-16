e996106ca1981a401ae8656911b249bd5e64ad50
801 Txchii <sdgaletech@gmail.com>
2023-03-16 16:46:12 +0000

    huh

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,77 +2,128 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
-	import Settings from './Settings.svelte'
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-</svelte:head>
-
 <header>
-	<div class='title'>
-		<h2>Deciphrase</h2>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
 	</div>
 
-	<div class="headerlinks link">
-		<div class="home">
-			<span aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">
-					<span class="material-symbols-outlined">
-						home
-					</span>
-				</a>
-			</span>
-		</div>
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
 
-		<div class="leaderboard link">
-			<span aria-current={$page.url.pathname === '/leaderboard' ? 'page' : undefined}>
-				<a href="/leaderboard">
-					<span class="material-symbols-outlined">
-						leaderboard
-					</span>
-				</a>
-			</span>
-		</div>
-
-		<div class="settingsicon link">
-			<Settings />
-		</div>
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
 	</div>
-
-	
-
 </header>
 
 <style>
-	.headerlinks {
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
 	}
 
-	.link {
-		margin: .5rem;
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
 	}
 
 	a:hover {
-		background: transparent;
-	}
-
-	.material-symbols-outlined {
-		font-variation-settings:
-		'FILL' 0,
-		'wght' 400,
-		'GRAD' 0,
-		'opsz' 48
-	}
-
-	.material-symbols-outlined:hover {
-		font-variation-settings:
-		'FILL' 1,
-		'wght' 400,
-		'GRAD' 0,
-		'opsz' 48
+		color: var(--color-theme-1);
 	}
 </style>
