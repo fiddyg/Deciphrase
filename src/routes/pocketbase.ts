ba16ef74bfd60dfbad730be6840177361bdc5eef
801 Txchii <sdgaletech@gmail.com>
2023-03-25 12:14:49 -0700

    sfdj

--- src/routes/pocketbase.ts
+++ src/routes/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase('http://23.239.22.25:443/')
+export const pb = new PocketBase('http://23.239.22.25:443')
 
 export const currentUser = writable(pb.authStore.model)
 
