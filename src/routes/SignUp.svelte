b4a59275262548fbb3e1dd9bc9bf4280c473135e
801 Txchii <sdgaletech@gmail.com>
2023-03-25 12:45:09 -0700

    ya

--- src/routes/SignUp.svelte
+++ src/routes/SignUp.svelte
@@ -1,47 +1,51 @@
-<script lang='ts'>
-    import { currentUser, pb } from "./pocketbase";
-
-    let username: string
-    let password: string 
-
+<script lang="ts">
+    import { currentUser, pb } from './pocketbase';
+    let username: string;
+    let password: string;
     async function login() {
-        await pb.collection('users').authWithPassword(username, password)
+      await pb.collection('users').authWithPassword(username, password);
     }
-
     async function signUp() {
-        try {
-            const data = {
-                username, 
-                password
-            }
-            const createdUser = await pb.collection('users').create(data)
-            await login()
-        } catch (err) {
-            console.log(err)
-        }
-
+      try {
+        const data = {
+          username,
+          password,
+          passwordConfirm: password,
+          name: 'hi mom!',
+        };
+        const createdUser = await pb.collection('users').create(data);
+        await login();
+      } catch (err) {
+        console.error(err)
+      }
     }
-
     function signOut() {
-        pb.authStore.clear()
+      pb.authStore.clear();
     }
 </script>
+  
+{#if $currentUser}
+    <p>
+        Signed in as {$currentUser.username} 
+        <button on:click={signOut}>Sign Out</button>
+    </p>
+{:else}
+    <form on:submit|preventDefault>
+        <input
+        placeholder="Username"
+        type="text"
+        bind:value={username}
+        />
 
-<main>
-    {#if $currentUser}
-        <p>Signed in as {$currentUser}</p>
-    {:else}
-        <form on:submit|preventDefault>
-            <input type="text" placeholder="Username" bind:value={username} required>
-
-            <input type="password" placeholder="Password" bind:value={password} required>
-
-            <button on:click={signUp}>Sign Up</button>
-
-            <button on:click={login}>Login</button>
-        </form>
-    {/if}
-</main>
+        <input 
+        placeholder="Password" 
+        type="password" 
+        bind:value={password} 
+        />
+        <button on:click={signUp}>Sign Up</button>
+        <button on:click={login}>Login</button>
+    </form>
+{/if}
 
 <style>
     form {
