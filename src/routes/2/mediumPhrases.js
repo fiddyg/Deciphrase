5f6475cbf95fe0d00f481552048c09857d3d6f8c
801 Txchii <sdgaletech@gmail.com>
2023-04-17 15:36:42 -0700

    hi

--- src/routes/2/mediumPhrases.js
+++ src/routes/2/mediumPhrases.js
@@ -6,7 +6,18 @@ import { shuffle } from "../phrases";
 
 const phrase1Word1Letters = ['e', 'a', 't']
 
-const phrase1Word1 = shuffle(phrase1Word1Letters)
+const phrase1Word1 = shuffle(phrase1Word1Letters).join('')
 
-const phrase1Word2Letters = ['my', 'y']
+const phrase1Word2Letters = ['m', 'y']
 
+const phrase1Word2 = shuffle(phrase1Word2Letters).join('')
+
+const phrase1Word3Letters = ['s', 'h', 'o', 'r', 't', 's']
+
+const phrase1Word3 = shuffle(phrase1Word3Letters).join('')
+
+const phrase1 = [phrase1Word1, phrase1Word2, phrase1Word3]
+
+
+
+const phrase2Word1Letters = ['i', "'", 'l', 'l']
